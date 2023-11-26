a7a96729b5135c31a86cb926f129ce9d40efab97
Fulano de Tal <gabriel.andrade200p@gmail.com>
2023-11-26 14:22:25 -0300

    mudando username

--- src/api/username.ts
+++ src/api/username.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import { z } from "zod";
 import { db } from "../lib/db";
 
 const router = express.Router();
 
-router.get<{}, any>("/", async (req, res) => {
-  const bodySchema = z.object({
-    email: z.string(),
-  });
-  const { email } = bodySchema.parse(req.body);
+router.post<{}, any>("/", async (req, res) => {
+  const { email } = req.body;
 
   const user = await db.user.findFirst({
     where: {
